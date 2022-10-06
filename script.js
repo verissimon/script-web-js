e69291990ec84b6cc06a2415e951d1ec6e6bcc6b
verissimo <terceiroverissimo@gmail.com>
2022-10-06 16:50:39 -0300

    sdfs

--- script.js
+++ script.js
@@ -9,12 +9,13 @@ function geradorInteiroUnico(){
 let inteiroUnico = geradorInteiroUnico();
 // console.log(inteiroUnico());
 // console.log(inteiroUnico());
-// console.log(inteiroUnico());
-// console.log(inteiroUnico());
+console.log(inteiroUnico());
+console.log(inteiroUnico());
 const soma = (x, y) => {
     return x + y;
 }
 const numeroAleatorio = () => parseInt(Math.random()*100);
 let a = numeroAleatorio();
+console.log(soma(4,5));
 //let a = soma(inteiroUnico(), inteiroUnico());
 console.log(a);
