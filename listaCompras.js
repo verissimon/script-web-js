db3edea8523d3138eab5857236b82194ee617a7e
IFPB <maciel.franks@academico.ifpb.edu.br>
2022-10-27 16:14:14 -0300

    aula 27/10

--- listaCompras.js
+++ listaCompras.js
@@ -6,12 +6,24 @@ let textoLista = document.querySelector("#elementoLista");
 // let textoNovoItem = document.createElement("span");
 // textoNovoItem.textContent = input.value;
 // textoLista.appendChild(textoNovoItem);
-
 let botao = document.querySelector(".btn");
 botao.addEventListener("click", function() {
-    console.log("click")
-    let textoNovoItem = document.createElement("span");
+    let textoNovoItem = document.createElement("div");
     textoNovoItem.classList.add("item-lista");
     textoNovoItem.textContent = input.value;
     textoLista.appendChild(textoNovoItem);
+    let rmv = document.createElement("button");
+    rmv.classList.add("rmv");
+    rmv.textContent = "Remover";
+    textoNovoItem.appendChild(rmv);
+    remover();
 });
+    function remover() {
+    let rmvBotao = document.getElementsByClassName("rmv");
+    rmvBotao.onclick = function() {
+        alert("clicou remover");
+        // element = document.querySelector(".item-lista");
+        // element.remove();
+        textoNovoItem.remove();
+    };
+    }
