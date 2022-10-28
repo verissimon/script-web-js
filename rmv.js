ce8bf4d77277800564a733ac67618773f7826b8a
verissimo terceiro <terceiroverissimo@gmail.com>
2022-10-28 00:24:51 -0300

    deletando rmv.js

--- rmv.js
+++
@@ -1,7 +0,0 @@
-let rmvBotao = document.getElementsByClassName("rmv");
-rmvBotao.addEventListener("click",function(){
-    alert("clicou remover");
-    // element = document.querySelector(".item-lista");
-    // element.remove();
-    textoNovoItem.removeChild(textoNovoItem);
-});
